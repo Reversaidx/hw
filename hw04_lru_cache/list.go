6d2cab3aa4d1c9c3f18422a2b0e7289763fc2af6
Dmitry F <dfedotov@tango.me>
2021-09-26 19:51:26 +0300

    bag fix

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -1,9 +1,5 @@
 package hw04lrucache
 
-import (
-	"errors"
-)
-
 type (
 	List interface {
 		Len() int
@@ -16,9 +12,6 @@ type (
 	}
 )
 
-var ErrInvalidSrtDst = errors.New("Dst index has to be more than srt")
-var ListMap = make(map[*ListItem]int)
-
 type ListItem struct {
 	Value interface{}
 	Next  *ListItem
@@ -26,88 +19,73 @@ type ListItem struct {
 }
 
 func (l list) Len() int {
-	return len(l.ListItems)
+	return l.len
 }
 func (l list) Back() *ListItem {
-	if l.Len() > 0 {
-		return l.ListItems[len(l.ListItems)-1]
-	}
-	return nil
+	return l.back
 }
 func (l list) Front() *ListItem {
-	if l.Len() > 0 {
-		return l.ListItems[0]
-	}
-	return nil
+	return l.front
 }
 func (l *list) PushBack(v interface{}) *ListItem {
-	tmp := &ListItem{Value: v, Next: nil, Prev: nil}
-	if l.Len() > 0 {
-		l.ListItems = append(l.ListItems, tmp)
-		l.bind(l.Len()-2, l.Len()-1)
-		l.UpdateMap(tmp, l.Len()-1-l.offset)
+	tmp := &ListItem{Value: v, Next: nil, Prev: l.back}
+	if l.len >= 1 {
+		l.back.Next = tmp
+		l.back = tmp
 	} else {
-		l.ListItems = append(l.ListItems, tmp)
-		l.UpdateMap(tmp, 0)
+		l.back = tmp
+		l.front = tmp
 	}
+
+	l.len++
 	return tmp
 }
 
 func (l *list) PushFront(v interface{}) *ListItem {
-	tmp := &ListItem{Value: v, Next: nil, Prev: nil}
-	if l.Len() > 0 {
-		tmpArr := make([]*ListItem, l.Len()+1)
-		tmpArr = append(tmpArr, tmp)
-
-		tmpArr = append(tmpArr, l.ListItems[:])
-		tmpArr[0] = tmp
-		l.bind(0, 1)
-
+	tmp := &ListItem{Value: v, Next: l.front, Prev: nil}
+	if l.len >= 1 {
+		l.front.Prev = tmp
+		l.front = tmp
 	} else {
-		l.ListItems = append(l.ListItems, tmp)
+		l.back = tmp
+		l.front = tmp
 	}
-
-	l.UpdateMap(tmp, 0)
-	l.offset++
+	l.len++
 	return tmp
 }
 
 func (l *list) Remove(i *ListItem) {
-	pos := l.Maps[i] + l.offset
-	l.bind(pos-1, pos+1)
-	l.ListItems = append(l.ListItems[:pos], l.ListItems[pos+1:]...)
 
+	if i.Prev != nil && i.Next != nil {
+		i.Prev.Next = i.Next.Prev
+		i.Next.Prev = i.Prev.Next
+	} else if i.Prev == nil && i.Next != nil {
+		i.Next.Prev = nil
+		l.front = i.Next
+	} else if i.Prev != nil && i.Next == nil {
+		i.Prev.Next = nil
+		l.back = i.Prev
+	} else if i.Prev == nil && i.Next == nil {
+		l.back = nil
+		l.front = nil
+	}
+
+	l.len--
 }
 
 func (l *list) MoveToFront(i *ListItem) {
 	l.Remove(i)
-	l.PushFront(i)
-
-}
-
-func (l *list) bind(srtPos, dstPos int) {
-	if srtPos > dstPos {
-		srtPos, dstPos = dstPos, srtPos
-	}
-	l.ListItems[srtPos].Next = l.ListItems[dstPos]
-	l.ListItems[dstPos].Prev = l.ListItems[srtPos]
+	l.PushFront(i.Value)
 }
 
 type list struct {
-	List      // Remove me after realization.
-	ListItems []*ListItem
-	Maps      map[*ListItem]int
-	offset    int
-
+	List  // Remove me after realization.
+	front *ListItem
+	back  *ListItem
+	len   int
 	// Place your code here.
 }
 
-func (l *list) UpdateMap(i *ListItem, pos int) {
-	if l.Maps == nil {
-		l.Maps = make(map[*ListItem]int)
-	}
-	l.Maps[i] = pos
-}
 func NewList() List {
 	return new(list)
 }
