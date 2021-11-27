f4136b77fbeb39032cdcb69cac4688d3670af744
Dmitry F <dfedotov@tango.me>
2021-11-27 17:43:18 +0300

    upd

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -11,43 +11,36 @@ import (
 )
 
 func main() {
-
-
-	directory:= os.Args[1]
-	cmd:=exec.Command(os.Args[2],os.Args[2:]...)
+	directory := os.Args[1]
+	cmd := exec.Command("/bin/bash", os.Args[3:]...)
 	files, err := ioutil.ReadDir(directory)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-
 	for _, file := range files {
+		content, err := ioutil.ReadFile(directory + "/" + file.Name())
+		if err != nil {
+			log.Fatal(err)
+		}
+		if len(content) == 0 {
+			os.Unsetenv(file.Name())
+		}
+		firstLine := strings.Split(string(content), "\n")[0]
+		contentT := strings.TrimRight(string(firstLine), " \t")
 
-		if file.Size()!=0{
-			content, err := ioutil.ReadFile(directory+"/"+file.Name())
-			if err != nil {
-				log.Fatal(err)
-			}
-			firstLine:=strings.Split(string(content), "\n")[0]
-			contentT:=strings.TrimRight(string(firstLine),"\t")
-
-			contentT:=string(bytes.Replace(byte(contentT),'\x00',byte("\n"),1000))
-			err=os.Setenv(file.Name(),contentT)
-			if err != nil {
-				fmt.Println(contentT)
-				log.Fatal(err)
-			}
-		}else {
-			err:=os.Unsetenv(file.Name())
-			if err != nil {
-				fmt.Println("kurwa")
-				log.Fatal(err)
-			}
+		contentT = string(bytes.Replace([]byte(contentT), []byte("\x00"), []byte("\n"), -1))
+		err = os.Setenv(file.Name(), contentT)
+		if err != nil {
+			fmt.Println(contentT)
+			log.Fatal(err)
 		}
 	}
 
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	if err := cmd.Run(); err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 }
-
