16a7a89d3836834be4bd74f04903fe7825919226
Dmitry F <dfedotov@tango.me>
2021-11-27 15:53:01 +0300

    upd

--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strings"
+)
+
 func main() {
-	// Place your code here.
+
+
+	directory:= os.Args[1]
+	cmd:=exec.Command(os.Args[2],os.Args[2:]...)
+	files, err := ioutil.ReadDir(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	for _, file := range files {
+
+		if file.Size()!=0{
+			content, err := ioutil.ReadFile(directory+"/"+file.Name())
+			if err != nil {
+				log.Fatal(err)
+			}
+			firstLine:=strings.Split(string(content), "\n")[0]
+			contentT:=strings.TrimRight(string(firstLine),"\t")
+
+			contentT:=string(bytes.Replace(byte(contentT),'\x00',byte("\n"),1000))
+			err=os.Setenv(file.Name(),contentT)
+			if err != nil {
+				fmt.Println(contentT)
+				log.Fatal(err)
+			}
+		}else {
+			err:=os.Unsetenv(file.Name())
+			if err != nil {
+				fmt.Println("kurwa")
+				log.Fatal(err)
+			}
+		}
+	}
+
+	if err := cmd.Run(); err != nil {
+		log.Fatal(err)
+	}
 }
+
