fcd82a4d5aa1eb6f006f4576f81f2a4f10f447d6
Dmitry F <dfedotov@tango.me>
2021-12-04 21:48:25 +0300

    upd

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,16 @@
 package main
 
+import (
+	"os"
+)
+
 func main() {
 	// Place your code here,
+	//var timeout *time.Duration
+	host := "127.0.0.1"
+	port := "4242"
+	//*timeout = time.Second * 5
+	//flag.DurationVar(timeout, "timeout", 0, "kurwa")
+	NewTelnetClient(host+":"+port, os.Stdin, os.Stdout)
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,14 @@
 package main
 
 import (
+	"bufio"
+	"context"
+	"fmt"
 	"io"
+	"log"
+	"net"
+	"os"
+	"sync"
 	"time"
 )
 
@@ -12,10 +19,84 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClien {
+	dialer := &net.Dialer{}
+	wg := &sync.WaitGroup{}
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	var client TelnetClient
+	conn, err := dialer.Dial("tcp", "127.0.0.1:4242")
+	stdin := stdinScan()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.WriteString("kurwa")
+	wg.Add(1)
+	go func() {
+		readRoutine(ctx, conn, wg)
+		cancel()
+	}()
+
+	wg.Add(1)
+	go func() {
+		writeRoutine(ctx, conn, wg, stdin)
+	}()
+	wg.Wait()
 	// Place your code here.
-	return nil
+	return client
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func stdinScan() chan string {
+	out := make(chan string)
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			out <- scanner.Text()
+		}
+		if scanner.Err() != nil {
+			close(out)
+		}
+	}()
+	return out
+}
+
+func readRoutine(ctx context.Context, conn net.Conn, wg *sync.WaitGroup) {
+	defer wg.Done()
+	scanner := bufio.NewScanner(conn)
+OUTER:
+	for {
+		select {
+		case <-ctx.Done():
+			break OUTER
+		default:
+			if !scanner.Scan() {
+				log.Printf("CANNOT SCAN")
+				break OUTER
+			}
+			text := scanner.Text()
+			log.Printf(text)
+		}
+	}
+	log.Printf("Finished readRoutine")
+}
+
+func writeRoutine(ctx context.Context, conn net.Conn, wg *sync.WaitGroup, stdin chan string) {
+	defer wg.Done()
+	//scanner := bufio.NewScanner(os.Stdin)
+OUTER:
+	for {
+		select {
+		case <-ctx.Done():
+			break OUTER
+		case str := <-stdin:
+			//if !scanner.Scan() {
+			//	break OUTER
+			//}
+			//str := scanner.Text()
+			//log.Printf("To server %v\n", str)
+
+			conn.Write([]byte(fmt.Sprintf("%s\n", str)))
+		}
+
+	}
+	log.Printf("Finished writeRoutine")
+}
